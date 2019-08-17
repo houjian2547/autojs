e52003cb3be4ba15f2918fe7e78eeb99aaa342b5
houjian2547 <41145541+houjian2547@users.noreply.github.com>
2019-08-17 16:11:06 +0800

    增加极速头条

--- main.js
+++ main.js
@@ -12,13 +12,15 @@ var module_zhongqingkandian = require('modules/zhongqingkandian.js');
 var module_diandianxinwen = require('modules/diandianxinwen.js');
 var module_ertoutiao = require('modules/ertoutiao.js');
 var module_guangyingxinwen = require('modules/guangyingxinwen.js');
+var module_jisutoutiao = require('modules/jisutoutiao.js');
 // var module_qutoutiao = require('modules/qutoutiao.js');
 // var module_huitoutiao = require('modules/huitoutiao.js');
 //各app模块
 var moduleNameArray = [module_zhifubao, module_shandianhezi,
     module_souhuzixun, module_shuabaoduanshipin,
     module_jukandian, module_xiangkan, module_weili, module_zhongqingkandian,
-    module_diandianxinwen,module_ertoutiao,module_guangyingxinwen];
+    module_diandianxinwen,module_ertoutiao,module_guangyingxinwen,
+    module_jisutoutiao];
 
 //============================== 全局变量=======================================
 
@@ -34,13 +36,14 @@ var appName_zhongqingkandian = "中青看点";
 var appName_diandianxinwen = "点点新闻";
 var appName_ertoutiao = "二头条";
 var appName_guangyingxinwen = "光影新闻";
+var appName_jisutoutiao = "极速头条";
 // var appName_qutoutiao = "趣头条";
 // var appName_huitoutiao = "惠头条";
 //可以选择的模块
 var appNameArray = [appName_zhifubao,
     appName_shandianhezi,appName_souhuzixun, appName_shuabaoduanshipin,
     appName_jukandian, appName_xiangkan, appName_weili, appName_zhongqingkandian, 
-    appName_diandianxinwen,appName_ertoutiao,appName_guangyingxinwen,
+    appName_diandianxinwen,appName_ertoutiao,appName_guangyingxinwen,appName_jisutoutiao,
     "随机应用"];
 
 //打开软件等待时间，单位秒
---
+++ modules/jisutoutiao.js
@@ -0,0 +1,83 @@
+var module_jisutoutiao = {};
+var commonFunction;
+//选择要启动的模块
+var firstPage_option = "首页";
+var searchKey = "tv_title";
+//计时id
+var timerId = "redpacket";
+
+//==============================程序启动区=======================================
+module_jisutoutiao.start = function (common) {
+    commonFunction = common;
+    //选择模块
+    scanArticle();
+}
+module_jisutoutiao.start_random = function (common) {
+    commonFunction = common;
+    selectArticle();
+}
+//=====================================scanArticle start===================================
+//浏览文章
+function scanArticle() {
+    sleep(2000);
+    while (true) {
+        selectArticle();
+    }
+}
+//选择某一篇文章
+function selectArticle() {
+    if (textEndsWith("拒绝").exists()) {
+        textEndsWith("拒绝").find().forEach(function (pos) {
+            var posb = pos.bounds();
+            if (posb.centerX() > 0 && posb.centerX() < 1000 && posb.centerY() > 0 && posb.centerY() < 1800) {
+                click(posb.centerX(), posb.centerY());
+                toastLog("点击了");
+            }
+        });
+    }
+    if (!id(searchKey).exists()) {
+        toastLog("文章不存在，滑动");
+        swipe(device.width / 2, device.height / 4 * 3, device.width / 2, device.height / 4, 2000);
+        return;
+    }
+    //遍历点击文章
+    toastLog(">>>>>>>>>>>当页开始<<<<<<<<<");
+    id(searchKey).find().forEach(function (pos) {
+        sleep(1000);
+        var posb = pos.bounds();
+        if (posb.centerX() > 0 && posb.centerX() < 1000 && posb.centerY() > 400 && posb.centerY() < 1800) {
+            // log("该条新闻中心坐标：centerX:" + posb.centerX() + ",centerY:" + posb.centerY());
+            click(posb.centerX(), posb.centerY());
+            toastLog("点击了文章，准备进入文章！");
+            sleep(2000);
+            //开始浏览文章
+            scanSingleArticle();
+            sleep(1000);
+        }
+    });
+    toastLog(">>>>>>>>>>>当页结束<<<<<<<<<");
+    swipe(device.width / 2, device.height / 4 * 3, device.width / 2, device.height / 4, 2000);
+}
+//=====================================scanSingleArticle===================================
+//文章内阅读循环
+function scanSingleArticle() {
+    if (!id(timerId).exists()) {
+        toastLog("不是文章");
+    } else {
+        toastLog("是文章");
+        sleep(10 * 1000);
+    }
+    if (textEndsWith(firstPage_option).exists()) {
+        toastLog("是首页");
+        return;
+    }
+    toastLog("不是首页，退出");
+    back();
+    sleep(1000);
+    if (!textEndsWith(firstPage_option).exists()) {
+        commonFunction.enterMainPage("极速头条");
+        this.scanArticle();
+    }
+}
+//=====================================end===================================
+module.exports = module_jisutoutiao;
